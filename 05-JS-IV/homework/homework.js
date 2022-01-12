e0f5442022db089ddf2b3fcea3406e51ac69739b
Maximiliano Rey <mrey@fiplasto.com.ar>
2022-01-12 10:12:44 -0300

    05-js-iv

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -130,6 +130,13 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+
+  var sumaLikes = 0;
+  for (var i=0; i<usuario.posts.length;i++){
+    sumaLikes=sumaLikes + usuario.posts[i].likes;
+  }
+  return sumaLikes;
+
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -142,6 +149,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  producto.calcularPrecioDescuento = function() {
+    return this.precio - ( this.precio * this.porcentajeDeDescuento );
+  };
+  return producto;
 
 }
 
